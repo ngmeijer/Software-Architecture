31a5f091a726ec081c197db34cf341e1d275a514
Nils Meijer <meijernils9@gmail.com>
2020-12-16 20:15:32 +0100

    Implemented first unit test

---
+++ Software Architecture/Assets/Editor/ItemFactoryTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class ItemFactoryTests
+{
+    [Test]
+    public void DisplayCorrectWeaponName()
+    {
+        ShopItemFactory factory = new ShopItemFactory();
+
+        Weapon weaponInstance = factory.CreateWeapon();
+
+        Assert.That(weaponInstance.Name, Is.EqualTo("low weapon"));
+    }
+}
